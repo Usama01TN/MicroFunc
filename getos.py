336f0bee88214c4382aaaa30ff17ef872f29a66c
Usama01TN <100040018+Usama01TN@users.noreply.github.com>
2025-02-05 20:54:23 +0100

    Update getos.py

--- getos.py
+++ getos.py
@@ -2,7 +2,7 @@
 """
 None
 """
-# For platform class
+# For platform class.
 from platform import system, machine
 from os import environ
 import sys
@@ -11,17 +11,23 @@ import sys
 class DetectOS(object):
     __osName = system().lower()
 
-    @classmethod
-    def isMobile(cls):
-        if system().lower().startswith("darwin") and cls.__osName in ["iphone", "ipad"]:
-            cls.__osName = 'ios'
+    @staticmethod
+    def isMobile():
+        """
+        :return: bool
+        """
+        if system().lower().startswith("darwin") and DetectOS.__osName in ["iphone", "ipad"]:
+            DetectOS.__osName = 'ios'  # type str
             return True
         elif hasattr(sys, 'getandroidapilevel') or 'ANDROID_BOOTLOGO' in environ or "aarch" in machine():
-            cls.__osName = 'android'
+            DetectOS.__osName = 'android'  # type str
             return True
         return False
 
-    @classmethod
-    def osName(cls):
-        cls.isMobile()
-        return cls.__osName
+    @staticmethod
+    def osName():
+        """
+        :return: str | unicode
+        """
+        DetectOS.isMobile()
+        return DetectOS.__osName
